235a034a0d4f9497f0e5bd5117e094580451c6ee
nemsissfr <nemsiss.shahbazian@firstresonance.io>
2023-07-17 21:01:51 -0700

    completed api server

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,33 +2,39 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from agents.linkedin_lookup_agent import lookup
+from output_parsers import person_intel_parser, PersonIntel
 
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from agents.twitter_lookup_agent import lookup as twitter_lookup
 
 name = "Eden Marco"
-if __name__ == "__main__":
-    print("Hello from langchain!")
 
+
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = lookup(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
     twitter_username = twitter_lookup(name=name)
     tweets = scrape_user_tweets(username=twitter_username)
     summary_template = """
-        given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-        3. a topic that may interest them
-        4. 2 creative ice breakers to start a conversation with them
-     """
+            given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+            3. a topic that may interest them
+            4. 2 creative ice breakers to start a conversation with them
+            \n{format_instructions} 
+         """
 
     # we specify that the input variable is called information
     # the "information" key corresponds to the {information} in summary template
+    # the partial variables is for formatting, so that we can format the LLM's response in either JSON or any other format and prepare it to send it back as a response to a frontend app
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     # the temperature determines how create the model will be, 0 means it won't be creative
@@ -38,4 +44,13 @@ if __name__ == "__main__":
     # the first arguement (llm) is actually the chatmodel which has the llm inside it (since chatmodels are wrappers around LLMs)
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    # print(result)
+
+    # return the parsed object
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+    print("Hello from langchain!")
+    ice_break(name="Eden Marco")
---
+++ output_parsers.py
@@ -0,0 +1,25 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="Summary about the person")
+    facts: List[str] = Field(description="Interesting facts about the person")
+    topics_of_interest: List[str] = Field(
+        description="Topics that may interest the person"
+    )
+    ice_breakers: List[str] = Field(
+        description="Create ice breakers to start a conversation with them"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "ice_breaker": self.ice_breakers,
+        }
+
+
+person_intel_parser: PydanticOutputParser = PydanticOutputParser(pydantic_object=PersonIntel)
